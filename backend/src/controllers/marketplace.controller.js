89609977c2ffeed926e564342b521168fb527d35
Roberto <rrodriguezesp@yahoo.es>
2026-05-13 15:22:32 +0200

    unified both getMarketPlaceUsers functions into one, also moved const{filters} into try

--- backend/src/controllers/marketplace.controller.js
+++ backend/src/controllers/marketplace.controller.js
@@ -2,64 +2,11 @@ import {prisma} from '../../prisma/prisma.js'
 
 
 //GET ALL ACCOUNTS = TALENT (user accounts with role 'TALENT'= students seeking job)
-export const  getMarketPlaceUsers = async (req , res) => {
-    try{
-        const users = await prisma.user.findMany({
-
-            where:{
-                role: 'TALENT'
-            },
-
-            include: {
-                skills:{
-                    include:{
-                        skill:true
-                    }
-                }
-            },
-            orderBy: {
-                createdAt: 'desc'
-            }
-        });
-
-        res.json(users);
-    }   catch(error){ 
-        res.status(500).json({
-            error:'INTERNAL_SERVER_ERROR'
-        });
-    }
-};
-
-//GET ONE TALENT BY Id, click profile => open
-export const getMarketPlaceUserById = async(req , res)=>{
-    try{
-        const {id} = req.params;
-        const user =await prisma.user.findUnique({
-            where :{id},
-
-            include: {
-                skills:{
-                    include:{
-                        skill:true
-                    }
-                }
-            }
-        });
-
-        if(!user){
-            return res.status(404).json({error: 'USER_NOT_FOUND'});
-        }
-        res.json(user);
-    } catch(error){
-        res.status(500).json({error: 'INTERNAL_SERVER_ERROR'});
-    }
-};
-
-//GET USERS BY FILTER SEARCH (skill, location, name)
+// + optional filters (applied to skill, name/surname, location)
 export const getMarketPlaceUsers = async (req ,res)=>{
-    const {skill,location,name} = req.query;
 
     try{
+        const {skill,location,name} = req.query;
         const users = await prisma.user.findMany({
             where : {
                 role : 'TALENT',
@@ -110,5 +57,31 @@ export const getMarketPlaceUsers = async (req ,res)=>{
         res.json(users);
     }   catch(error){
         res.status(500).json({error: 'INTERNAL_SERVER_ERROR'})
+    }
+};
+
+
+//GET ONE TALENT BY Id, click profile => open
+export const getMarketPlaceUserById = async(req , res)=>{
+    try{
+        const {id} = req.params;
+        const user =await prisma.user.findUnique({
+            where :{id},
+
+            include: {
+                skills:{
+                    include:{
+                        skill:true
+                    }
+                }
+            }
+        });
+
+        if(!user){
+            return res.status(404).json({error: 'USER_NOT_FOUND'});
+        }
+        res.json(user);
+    } catch(error){
+        res.status(500).json({error: 'INTERNAL_SERVER_ERROR'});
     }
 };
