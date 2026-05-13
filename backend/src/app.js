f8b61fc1717205d56c825b3ece8c10e77df0281d
mavagoncalves <mavagoncalves@gmail.com>
2026-05-13 23:57:45 +0200

    Adding routes to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import { prisma } from "../prisma/prisma.js";
 import authRoutes from "./routes/auth.routes.js";
 import profileRoutes from "./routes/profile.routes.js";
 import marketplaceRoutes from './routes/marketplace.routes';
+import reviewRoutes from "./routes/review.routes.js";
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.get("/health", (_req, res) => {
 
 app.use("/profiles", profileRoutes);
 app.use("/auth", authRoutes);
+app.use("/reviews", reviewRoutes);
 
 app.get("/users", async (_req, res, next) => {
   try {
