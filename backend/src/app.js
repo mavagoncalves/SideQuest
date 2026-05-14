7d0816bc33f4cab903553cf68441131152464ca3
mavagoncalves <mavagoncalves@gmail.com>
2026-05-14 16:33:02 +0200

    Fix: typo on import

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import morgan from "morgan";
 import { prisma } from "../prisma/prisma.js";
 import authRoutes from "./routes/auth.routes.js";
 import profileRoutes from "./routes/profile.routes.js";
-import marketplaceRoutes from './routes/marketplace.routes';
+import marketplaceRoutes from './routes/marketplace.routes.js';
 import reviewRoutes from "./routes/review.routes.js";
 
 const app = express();
