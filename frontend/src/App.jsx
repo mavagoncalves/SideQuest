f8f2b82c60e2d2b44206c4d5488fd2c8e91227a1
mavagoncalves <mavagoncalves@gmail.com>
2026-05-14 15:55:43 +0200

    Adapting profilePage to handle user settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import ServiceDetail from './pages/ServiceDetail'
 import ProfilePage from './pages/ProfilePage'
 import SignupPage from './pages/SignupPage'
 import PublicProfile from './pages/PublicProfile'
+import SettingsPage from "./pages/SettingsPage.jsx";
 
 const App =() =>{
 
@@ -19,8 +20,9 @@ const App =() =>{
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/service/create' element={<CreateServicePage/>}/>
         <Route path='/signup' element={<SignupPage/>}/>
-        <Route path='/public-profile' element={<PublicProfile/>}/>
+        <Route path='/profile:id' element={<ProfilePage/>}/>
         <Route path="/service/:id" element={<ServiceDetail />} />
+        <Route path="/settings" element={<SettingsPage />} />
         
 
         {/* Only logged in users can access these */}
